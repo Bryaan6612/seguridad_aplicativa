5a7e886a6cbaa329106f00d89118ccb431aa08e7
Bryaan6612 <129255325+Bryaan6612@users.noreply.github.com>
2023-08-30 19:33:32 -0400

    Add files via upload

---
+++ Dia3/Actividad4.py
@@ -0,0 +1,40 @@
+def promedio(alumnos):
+    total = len(alumnos)
+    suma = 0
+    for i in alumnos:
+        suma = alumnos[i] + suma
+    print("El promedio es: ", (suma/total))
+        
+def mayor(alumnos):
+    alumno
+
+def menor():
+        if(str(calificacion) <= 5 ):
+            print("Alumnos Reprobados: " + calificacion + alumnos[calificacion])
+            
+
+
+alumnos ={
+    "Juan":8,
+    "Giselle":9,
+    "Damian":5,
+    "Ricardo":6,
+    "Yaotzin":6,
+    "Erick":7,
+    "Mario":8,
+    "Edgar":9,
+    "Fernanda":5,
+    "Daniel":6,
+    "Jesus":7,
+    "Damian":8,
+    "Yemahina":6,
+    "Eduardo":9,
+    "Bryan":9,
+    "Mariano":10,
+    "Alberto":8
+    }
+
+promedio(alumnos)
+mayor(alumnos)
+
+
